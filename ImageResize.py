cc03146d724d5bd6aa032fb94b77ddeef996c710
LGobin <luke.gobin1@icloud.com>
2022-01-04 17:36:06 -0600

    Add Comments to ImageResize.py
    Also removed redundant code

--- ImageResize.py
+++ ImageResize.py
@@ -1,25 +1,25 @@
 import os
 import pathlib
 import sys
-
 from PIL import Image
 
+# Fetches absolute file path
 dest_path = pathlib.Path().resolve()
 
-
+# Checks if Program is opened with an image
 if len(sys.argv) > 1:
     for i in range(1, len(sys.argv)):
 
-        if i == sys.argv[0]:
-            continue
-
+        # Opens an image from ARGV list
         image = Image.open(sys.argv[i])
         print("current image size: ", image.size)
 
         width, height = image.size
+        # Multiply image width and height by 10
         resized_image = image.resize((width * 10, height * 10), Image.ANTIALIAS)
-        print("new image size:", resized_image.size)
+        print("new image size: ", resized_image.size)
 
+        # Saves image to user's current working directory
         resized_image.save(os.path.join(dest_path, image.filename))
 
         image.close()
